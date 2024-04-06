eb418d7d2401d35da416765c08f55b9f85402545
GustavoPellanda <gusfipe@gmail.com>
2024-04-05 21:29:44 -0300

    Lista de Alunos criada.

--- compile_and_run.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-g++ -I./include -o Sistema_Academico main.cpp src/Pessoa.cpp src/Universidade.cpp src/Principal.cpp src/Departamento.cpp src/Professor.cpp src/Aluno.cpp src/Disciplina.cpp src/ElementoAluno.cpp src/ListaAlunos.cpp
-
-Sistema_Academico.exe
