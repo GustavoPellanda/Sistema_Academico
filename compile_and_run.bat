549aee8b6693f0c9f401fa158bc5470afdf3dc5c
GustavoPellanda <gusfipe@gmail.com>
2024-05-16 02:11:55 -0300

    Template para elementos de listas criado.

--- compile_and_run.bat
+++ compile_and_run.bat
@@ -1,5 +1,5 @@
 @echo off
 
-g++ -I./include -o Sistema_Academico main.cpp src/Pessoa.cpp src/Universidade.cpp src/Principal.cpp src/Departamento.cpp src/Professor.cpp src/Aluno.cpp src/Disciplina.cpp src/ElementoAluno.cpp src/ListaAlunos.cpp src/Menu.cpp
+g++ -I./include -I./templates -o Sistema_Academico main.cpp src/*.cpp
 
 Sistema_Academico.exe
---
+++ templates/ElementoDeUmaLista.h
@@ -0,0 +1,67 @@
+#ifndef _ELEMENTODEUMALISTA_H_
+#define _ELEMENTODEUMALISTA_H_
+
+template<class TIPO>
+class ElementoLista{
+private:
+    TIPO* pInfoElemento;    
+    ElementoLista<TIPO>* pProximoElemento;
+    ElementoLista<TIPO>* pAnteriorElemento;
+
+public:
+    ElementoLista();
+    ~ElementoLista();
+
+    void setInfo(TIPO* pInfoEle);
+    TIPO* getInfo();
+
+    void setProximoElemento(ElementoLista<TIPO>* pProxEle);
+    ElementoLista<TIPO>* getProximoElemento();
+
+    void setAnteriorElemento(ElementoLista<TIPO>* pAnteEle);
+    ElementoLista<TIPO>* getAnteriorElemento();
+};
+
+template<class TIPO>
+ElementoLista<TIPO>::ElementoLista(){
+    pInfoElemento = NULL;
+    pProximoElemento = NULL;
+    pAnteriorElemento = NULL;
+}
+
+template<class TIPO>
+ElementoLista<TIPO>::~ElementoLista(){
+    delete pInfoElemento;
+}
+
+template<class TIPO>
+void ElementoLista<TIPO>::setInfo(TIPO* pInfoEle){
+    pInfoElemento = pInfoEle;
+}
+
+template<class TIPO>
+TIPO* ElementoLista<TIPO>::getInfo(){
+    return pInfoElemento;
+}
+
+template<class TIPO>
+void ElementoLista<TIPO>::setProximoElemento(ElementoLista<TIPO>* pProxEle){
+    pProximoElemento = pProxEle;
+}
+
+template<class TIPO>
+ElementoLista<TIPO>* ElementoLista<TIPO>::getProximoElemento(){
+    return pProximoElemento;
+}
+
+template<class TIPO>
+void ElementoLista<TIPO>::setAnteriorElemento(ElementoLista<TIPO>* pAnteEle){
+    pAnteriorElemento = pAnteEle;
+}
+
+template<class TIPO>
+ElementoLista<TIPO>* ElementoLista<TIPO>::getAnteriorElemento(){
+    return pAnteriorElemento;
+}
+
+#endif
